fb27b090164c6e799eafbc26431a3a88ece9fe3d
Hamun Javadzadeghan <116430962+Hamuon@users.noreply.github.com>
2022-11-12 14:43:55 +0330

    Add files via upload

---
+++ Personal-Blog-CV-HTML-CSS-JS-main/README.md
@@ -0,0 +1,2 @@
+# Personal-Blog-CV-HTML-CSS-JS
+A simple CV profile using HTML, CSS and a little bit of JS code 
---
+++ Personal-Blog-CV-HTML-CSS-JS-main/app.js
@@ -0,0 +1,9 @@
+const toggler = document.querySelector(".toggler");
+const navbar = document.querySelector(".navbar");
+toggler.addEventListener("click", (e) => {
+  navbar.classList.toggle("nav__expanded");
+});
+
+
+
+
